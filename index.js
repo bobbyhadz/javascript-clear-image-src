9dc3548ca396f34556082f1b88daf24da5282830
bobbyhadz <opmftw@gmail.com>
2024-03-05 14:22:30 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+console.log('bobbyhadz.com');
+
+const img = document.getElementById('img');
+
+// 👇️ set image src attribute to an empty string
+img.setAttribute('src', '');
+
+// 👇️ or hide image
+img.style.display = 'none';
